3b249adc329dbf16dbeb2a79089dca514211db96
ChrisNercessian <90212134+ChrisNercessian@users.noreply.github.com>
2022-02-26 12:13:48 -0500

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -13,7 +13,7 @@ class Sheep {
     updateNeibourghs() {
         this.neighbours = [
             [this.x - 1, this.y - 1],
-            [this.x, y - this.y - 1],
+            [this.x, this.y - 1],
             [this.x + 1, this.y - 1],
             [this.x - 1, this.y],
             [this.x + 1, this.y],
